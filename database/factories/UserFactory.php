bc0fd7f4205efc0f9cbc8f665389be9a90da8f51
joachim <joachim.vanmeirvenne@atheneumkapellen.be>
2026-04-25 17:03:33 +0200

    Deploy update Sat Apr 25 05:03:33 PM CEST 2026

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,11 +25,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'User ' . \Illuminate\Support\Str::random(5),
+            'email' => \Illuminate\Support\Str::random(10) . '@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'remember_token' => \Illuminate\Support\Str::random(10),
         ];
     }
 

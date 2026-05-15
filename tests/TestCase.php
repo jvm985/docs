850566e0230a8d6602d4da45809269f874fc3999
joachim <joachim.vanmeirvenne@atheneumkapellen.be>
2026-05-15 12:36:35 +0200

    debug(tests): log traits applied to test classes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        $traits = class_uses_recursive(static::class);
+        fwrite(STDERR, "[DEBUG] TestCase setUp class=".static::class." traits=".implode(',', array_keys($traits))."\n");
+        parent::setUp();
+        fwrite(STDERR, "[DEBUG] After parent::setUp; schema has users? ".(\Illuminate\Support\Facades\Schema::hasTable('users') ? 'yes' : 'NO')."\n");
+    }
 }
